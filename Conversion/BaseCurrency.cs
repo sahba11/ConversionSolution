0e177e37b88faaa702c8455c034102d820952b48
Saba <Saba@DESKTOP-UH0LFB7>
2022-10-11 10:29:51 -0700

    Add project files.

---
+++ Conversion/BaseCurrency.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conversion
+{
+    internal class BaseCurrency : ICurrencyConverter
+    {
+        public void ClearConfiguration()
+        {
+            throw new NotImplementedException();
+        }
+
+        public double Convert(string fromCurrency, string toCurrency, double amount)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateConfiguration(IEnumerable<Tuple<string, string, double>> conversionRates)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
